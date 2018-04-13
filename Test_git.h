c3feef72a8793656fc75928be984e641e704f62d
Kmicice <36668721+Kmicice@users.noreply.github.com>
2018-04-13 15:33:11 +0200

    Testing
    Adding first files

---
+++ Test_git.h
@@ -0,0 +1,17 @@
+
+#ifndef Date_h
+#define Date_h
+#include<string>
+using namespace std;
+class date {
+	int rok;
+	string miesiac;
+	string dzien;
+	string godzina;
+
+	date(string, string, string, string);
+	date();
+};
+
+
+#endif // 
